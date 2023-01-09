aa6f94a73e13e098bb4a424a09ab6e00ac134241
Pedra96 <darkraidialga@yahoo.it>
2023-01-09 15:50:14 +0100

    Add project files.

---
+++ La Mia Pizzeria 1/Controllers/PizzaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace La_Mia_Pizzeria_1.Controllers {
+    public class PizzaController : Controller {
+        public IActionResult Index() {
+            List<Pizza> listaDeiPost = PostData.GetPosts();
+
+            return View("Index", listaDeiPost);
+            return View();
+        }
+    }
+}
---
+++ La Mia Pizzeria 1/Models/Pizza.cs
@@ -0,0 +1,17 @@
+﻿namespace La_Mia_Pizzeria_1.Models {
+    public class Pizza {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        public Pizza(int id, string title, string description, string image) {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+        }
+
+    }
+}
+
